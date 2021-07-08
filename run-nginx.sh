1a14f4fc9d6f51a08e18f3edcaf2e0110f8c0bf7
Willian Mitsuda <wmitsuda@gmail.com>
2021-07-08 01:50:55 -0300

    Deprecate backticks

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-PARAMS=`echo "{\"erigonURL\": \"$ERIGON_URL\"}" | jq -aRs .`
+PARAMS=$(echo "{\"erigonURL\": \"$ERIGON_URL\"}" | jq -aRs .)
 echo $PARAMS > /usr/share/nginx/html/config.json
 nginx -g "daemon off;"
