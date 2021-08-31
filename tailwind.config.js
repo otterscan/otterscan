5f9ec5e9927ea83612cdf8c35b2077ee76a0d1f3
Willian Mitsuda <wmitsuda@gmail.com>
2021-08-31 03:44:26 -0300

    Start theme extraction work; extract from/to colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 const colors = require("tailwindcss/colors");
 
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
@@ -18,6 +27,22 @@ module.exports = {
         data: ["Roboto Mono"],
         balance: ["Fira Code"],
         blocknum: ["Roboto"],
+      },
+      borderColor: {
+        skin: {
+          from: withOpacity("--color-from-border"),
+        },
+      },
+      textColor: {
+        skin: {
+          from: withOpacity("--color-from-text"),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          from: withOpacity("--color-from-fill"),
+          to: withOpacity("--color-to-fill"),
+        },
       },
     },
   },
