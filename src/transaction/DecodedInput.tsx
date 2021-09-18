c7c9f386177b77b94c872c2ff950e6eb02f5d425
Willian Mitsuda <wmitsuda@gmail.com>
2021-09-18 15:52:33 -0300

    Fix display of bool input/event params; fix grid x spacing

--- src/transaction/DecodedInput.tsx
+++ src/transaction/DecodedInput.tsx
@@ -8,7 +8,7 @@ type DecodedInputProps = {
 const DecodedInput: React.FC<DecodedInputProps> = ({ txDesc }) => (
   <table className="border rounded">
     <thead>
-      <tr className="grid grid-cols-12 text-left gap-x-1 py-2 bg-gray-100">
+      <tr className="grid grid-cols-12 text-left gap-x-2 py-2 bg-gray-100">
         <th className="col-span-3 pl-1">
           param <span className="text-gray-400 text-xs">(index)</span>
         </th>
@@ -18,7 +18,7 @@ const DecodedInput: React.FC<DecodedInputProps> = ({ txDesc }) => (
     </thead>
     <tbody className="divide-y">
       {txDesc.args.map((r, i) => (
-        <tr key={i} className="grid grid-cols-12 gap-x-1 py-2">
+        <tr key={i} className="grid grid-cols-12 gap-x-2 py-2">
           <td className="col-span-3 pl-1">
             {txDesc.functionFragment.inputs[i].name}{" "}
             <span className="text-gray-400 text-xs">({i})</span>
@@ -26,7 +26,9 @@ const DecodedInput: React.FC<DecodedInputProps> = ({ txDesc }) => (
           <td className="col-span-1">
             {txDesc.functionFragment.inputs[i].type}
           </td>
-          <td className="col-span-8 pr-1 font-code break-all">{r}</td>
+          <td className="col-span-8 pr-1 font-code break-all">
+            {r.toString()}
+          </td>
         </tr>
       ))}
     </tbody>
--- src/transaction/DecodedLog.tsx
+++ src/transaction/DecodedLog.tsx
@@ -8,7 +8,7 @@ type DecodedLogProps = {
 const DecodedLog: React.FC<DecodedLogProps> = ({ logDesc }) => (
   <table className="border rounded">
     <thead>
-      <tr className="grid grid-cols-12 text-left gap-x-1 py-2 bg-gray-100">
+      <tr className="grid grid-cols-12 text-left gap-x-2 py-2 bg-gray-100">
         <th className="col-span-3 pl-1">
           param <span className="text-gray-400 text-xs">(index)</span>
         </th>
@@ -18,13 +18,15 @@ const DecodedLog: React.FC<DecodedLogProps> = ({ logDesc }) => (
     </thead>
     <tbody className="divide-y">
       {logDesc.args.map((r, i) => (
-        <tr key={i} className="grid grid-cols-12 gap-x-1 py-2">
+        <tr key={i} className="grid grid-cols-12 gap-x-2 py-2">
           <td className="col-span-3 pl-1">
             {logDesc.eventFragment.inputs[i].name}{" "}
             <span className="text-gray-400 text-xs">({i})</span>
           </td>
           <td className="col-span-1">{logDesc.eventFragment.inputs[i].type}</td>
-          <td className="col-span-8 pr-1 font-code break-all">{r}</td>
+          <td className="col-span-8 pr-1 font-code break-all">
+            {r.toString()}
+          </td>
         </tr>
       ))}
     </tbody>
