0ede7cf3c9e9ab1e1b278376220f20d3a36b1cf6
Willian Mitsuda <wmitsuda@gmail.com>
2021-09-23 21:27:40 -0300

    Migrate deprecated attribute activeClassname

--- src/components/NavTab.tsx
+++ src/components/NavTab.tsx
@@ -6,13 +6,16 @@ type NavTabProps = {
   href: string;
 };
 
-// TODO: migrate activeClassName because of: https://github.com/remix-run/react-router/releases/tag/v5.3.0
-// TODO: @types/react-router-dom still doesn't support function in className
 const NavTab: React.FC<NavTabProps> = ({ href, children }) => (
   <Tab as={Fragment}>
     <NavLink
-      className="text-gray-500 border-transparent hover:text-link-blue text-sm font-bold px-3 py-3 border-b-2"
-      activeClassName="text-link-blue border-link-blue"
+      className={(isActive) =>
+        `${
+          isActive
+            ? "text-link-blue border-link-blue"
+            : "text-gray-500 border-transparent"
+        } hover:text-link-blue text-sm font-bold px-3 py-3 border-b-2`
+      }
       to={href}
       exact
       replace
