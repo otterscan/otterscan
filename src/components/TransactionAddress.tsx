128c70b972cb60136428a428f0793fbb0fcef304
Willian Mitsuda <wmitsuda@gmail.com>
2021-11-07 08:56:12 -0300

    Replace component

--- src/components/TransactionAddress.tsx
+++ src/components/TransactionAddress.tsx
@@ -3,14 +3,17 @@ import AddressHighlighter from "./AddressHighlighter";
 import DecoratedAddressLink from "./DecoratedAddressLink";
 import { ResolvedAddresses } from "../api/address-resolver";
 import { useSelectedTransaction } from "../useSelectedTransaction";
+import { AddressContext } from "../types";
 
 type TransactionAddressProps = {
   address: string;
+  addressCtx?: AddressContext | undefined;
   resolvedAddresses: ResolvedAddresses | undefined;
 };
 
 const TransactionAddress: React.FC<TransactionAddressProps> = ({
   address,
+  addressCtx,
   resolvedAddresses,
 }) => {
   const txData = useSelectedTransaction();
@@ -21,6 +24,7 @@ const TransactionAddress: React.FC<TransactionAddressProps> = ({
     <AddressHighlighter address={address}>
       <DecoratedAddressLink
         address={address}
+        addressCtx={addressCtx}
         miner={address === txData?.confirmedData?.miner}
         txFrom={address === txData?.from}
         txTo={address === txData?.to || creation}
