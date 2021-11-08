445cd679d8a732842d15179e5eff65f1cf4a12fb
Willian Mitsuda <wmitsuda@gmail.com>
2021-11-08 14:57:51 -0300

    Tweak styling of disabled tabs

--- src/components/ModeTab.tsx
+++ src/components/ModeTab.tsx
@@ -10,7 +10,7 @@ const ModeTab: React.FC<ModeTabProps> = ({ disabled, children }) => (
     className={({ selected }) =>
       `border rounded-lg px-2 py-1 bg-gray-100 ${
         disabled
-          ? "text-gray-300"
+          ? "border-gray-100 text-gray-300 cursor-default"
           : "hover:bg-gray-200 hover:shadow text-gray-500 hover:text-gray-600"
       } text-xs ${selected ? "border-blue-300" : ""}`
     }
