e640d11a16e5518c58311f46155c825a3203a96f
Willian Mitsuda <wmitsuda@gmail.com>
2021-11-21 09:04:08 -0300

    Fix ERC resolver for tokens with empty name/symbol

--- src/api/address-resolver/ERCTokenResolver.ts
+++ src/api/address-resolver/ERCTokenResolver.ts
@@ -11,11 +11,17 @@ export class ERCTokenResolver implements IAddressResolver<TokenMeta> {
   ): Promise<TokenMeta | undefined> {
     const erc20Contract = new Contract(address, erc20, provider);
     try {
-      const [name, symbol, decimals] = await Promise.all([
+      const [name, symbol, decimals] = (await Promise.all([
         erc20Contract.name(),
         erc20Contract.symbol(),
         erc20Contract.decimals(),
-      ]);
+      ])) as [string, string, number];
+
+      // Prevent faulty tokens with empty name/symbol
+      if (!name.trim() || !symbol.trim()) {
+        return undefined;
+      }
+
       return {
         name,
         symbol,
