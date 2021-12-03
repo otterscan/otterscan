6a15285a48844c3603caafff5d17c5f0d0f2ae00
Willian Mitsuda <wmitsuda@gmail.com>
2021-12-03 10:04:42 -0300

    Add uniswap V1 support

--- src/api/address-resolver/index.ts
+++ src/api/address-resolver/index.ts
@@ -2,6 +2,7 @@ import { BaseProvider } from "@ethersproject/providers";
 import { ensRenderer } from "../../components/ENSName";
 import { plainStringRenderer } from "../../components/PlainString";
 import { tokenRenderer } from "../../components/TokenName";
+import { uniswapV1PairRenderer } from "../../components/UniswapV1ExchangeName";
 import { uniswapV2PairRenderer } from "../../components/UniswapV2PairName";
 import { IAddressResolver, ResolvedAddressRenderer } from "./address-resolver";
 import {
@@ -9,6 +10,7 @@ import {
   SelectedResolvedName,
 } from "./CompositeAddressResolver";
 import { ENSAddressResolver } from "./ENSAddressResolver";
+import { UniswapV1Resolver } from "./UniswapV1Resolver";
 import { UniswapV2Resolver } from "./UniswapV2Resolver";
 import { ERCTokenResolver } from "./ERCTokenResolver";
 import { HardcodedAddressResolver } from "./HardcodedAddressResolver";
@@ -18,12 +20,14 @@ export type ResolvedAddresses = Record<string, SelectedResolvedName<any>>;
 // Create and configure the main resolver
 export const ensResolver = new ENSAddressResolver();
 export const uniswapV2Resolver = new UniswapV2Resolver();
+export const uniswapV1Resolver = new UniswapV1Resolver();
 export const ercTokenResolver = new ERCTokenResolver();
 export const hardcodedResolver = new HardcodedAddressResolver();
 
 const _mainResolver = new CompositeAddressResolver();
 _mainResolver.addResolver(ensResolver);
 _mainResolver.addResolver(uniswapV2Resolver);
+_mainResolver.addResolver(uniswapV1Resolver);
 _mainResolver.addResolver(ercTokenResolver);
 _mainResolver.addResolver(hardcodedResolver);
 
@@ -36,6 +40,7 @@ export const resolverRendererRegistry = new Map<
 >();
 resolverRendererRegistry.set(ensResolver, ensRenderer);
 resolverRendererRegistry.set(uniswapV2Resolver, uniswapV2PairRenderer);
+resolverRendererRegistry.set(uniswapV1Resolver, uniswapV1PairRenderer);
 resolverRendererRegistry.set(ercTokenResolver, tokenRenderer);
 resolverRendererRegistry.set(hardcodedResolver, plainStringRenderer);
 
