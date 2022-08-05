eb9b5f0607f6ff04c193bf8e20f90594cd32ff68
Willian Mitsuda <wmitsuda@gmail.com>
2022-08-05 05:40:49 -0300

    Apply code formatter

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteCompression from 'vite-plugin-compression';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression()]
-})
+  plugins: [react(), viteCompression()],
+});
