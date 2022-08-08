664106b542e5a13fc39f49eba96469eed98224a8
Willian Mitsuda <wmitsuda@gmail.com>
2022-08-08 04:59:31 -0300

    Reference type definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ function withOpacity(variableName) {
   };
 }
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
