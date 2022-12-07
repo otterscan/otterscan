9513b7dde71cb6cacb56ed7edc4b5f4d8501f895
Willian Mitsuda <wmitsuda@gmail.com>
2022-12-07 16:27:52 -0300

    Extract subcomponent

--- src/consensus/epoch/SlotItem.tsx
+++ src/consensus/epoch/SlotItem.tsx
@@ -1,13 +1,8 @@
 import { FC, memo } from "react";
-import { commify } from "@ethersproject/units";
 import SuspendedSlotItem from "./SuspendedSlotItem";
-import SlotLink from "../components/SlotLink";
-import SlotTimestamp from "./SlotTimestamp";
-import ValidatorLink from "../components/ValidatorLink";
-import BlockRoot from "../slot/BlockRoot";
-import AggregationParticipation from "../slot/AggregationParticipation";
-import { useFinalizedSlotNumber, useSlot } from "../../useConsensus";
 import NotFoundSlotItem from "./NotFoundSlotItem";
+import StoredSlotItem from "./StoredSlotItem";
+import { useFinalizedSlotNumber, useSlot } from "../../useConsensus";
 
 type SlotItemProps = {
   slotNumber: number;
@@ -31,32 +26,7 @@ const SlotItem: FC<SlotItemProps> = ({ slotNumber }) => {
     return <NotFoundSlotItem slotNumber={slotNumber} missed />;
   }
 
-  return (
-    <div className="grid grid-cols-12 gap-x-1 items-baseline text-sm border-t border-gray-200 hover:bg-skin-table-hover px-2 py-3">
-      <SlotLink slotNumber={slotNumber} />
-      <div>Proposed</div>
-      <div className="truncate">
-        <SlotTimestamp slotNumber={slotNumber} />
-      </div>
-      <ValidatorLink validatorIndex={slot.data.message.proposer_index} />
-      <div className="truncate">
-        <BlockRoot slotNumber={slotNumber} />
-      </div>
-      <div>
-        {commify(slot.data.message.body.attestations.length.toString())}
-      </div>
-      <div className="col-span-2">
-        <AggregationParticipation
-          hex={slot.data.message.body.sync_aggregate.sync_committee_bits}
-        />
-      </div>
-      <div>{commify(slot.data.message.body.deposits.length.toString())}</div>
-      <div></div>
-      <div>
-        {commify(slot.data.message.body.voluntary_exits.length.toString())}
-      </div>
-    </div>
-  );
+  return <StoredSlotItem slotNumber={slotNumber} slot={slot} />;
 };
 
 export default memo(SlotItem);
