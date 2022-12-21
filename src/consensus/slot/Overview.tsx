64bee1298e6044053d6d39b0e73d855931765df1
Willian Mitsuda <wmitsuda@gmail.com>
2022-12-21 03:46:33 -0300

    Linkify ETH1 block hash field

--- src/consensus/slot/Overview.tsx
+++ src/consensus/slot/Overview.tsx
@@ -85,7 +85,7 @@ const Overview: FC = () => {
             <HexValue value={slot.data.message.body.graffiti} />)
           </InfoRow>
           <InfoRow title="ETH1 Block Hash">
-            <HexValue value={slot.data.message.body.eth1_data.block_hash} />
+            <BlockLink blockTag={slot.data.message.body.eth1_data.block_hash} />
           </InfoRow>
           <InfoRow title="ETH1 Deposit Root">
             <HexValue value={slot.data.message.body.eth1_data.deposit_root} />
