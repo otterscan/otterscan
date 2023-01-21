46ba92a1601778fc746f5cfa3dac22945a96974a
Willian Mitsuda <wmitsuda@gmail.com>
2023-01-21 19:45:17 -0300

    Extract subtitle component

--- src/AddressMainPage.tsx
+++ src/AddressMainPage.tsx
@@ -7,15 +7,12 @@ import {
   useSearchParams,
 } from "react-router-dom";
 import { Tab } from "@headlessui/react";
-import Blockies from "react-blockies";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 import { faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
 import StandardFrame from "./StandardFrame";
-import StandardSubtitle from "./StandardSubtitle";
+import AddressSubtitle from "./address/AddressSubtitle";
 import AddressOrENSNameNotFound from "./components/AddressOrENSNameNotFound";
-import Copy from "./components/Copy";
-import Faucet from "./components/Faucet";
 import NavTab from "./components/NavTab";
 import SourcifyLogo from "./sourcify/SourcifyLogo";
 import AddressTransactionResults from "./address/AddressTransactionResults";
@@ -23,14 +20,13 @@ import AddressTokens from "./address/AddressTokens";
 import Contracts from "./address/Contracts";
 import { RuntimeContext } from "./useRuntime";
 import { useHasCode } from "./useErigonHooks";
-import { useChainInfo } from "./useChainInfo";
 import { useAddressOrENS } from "./useResolvedAddresses";
 import { useSourcifyMetadata } from "./sourcify/useSourcify";
 import { ChecksummedAddress } from "./types";
 
 type AddressMainPageProps = {};
 
-const AddressMainPage: React.FC<AddressMainPageProps> = ({}) => {
+const AddressMainPage: React.FC<AddressMainPageProps> = () => {
   const { addressOrName, direction } = useParams();
   if (addressOrName === undefined) {
     throw new Error("addressOrName couldn't be undefined here");
@@ -61,8 +57,6 @@ const AddressMainPage: React.FC<AddressMainPageProps> = ({}) => {
     provider?.network.chainId
   );
 
-  const { network, faucets } = useChainInfo();
-
   useEffect(() => {
     if (isENS || checksummedAddress === undefined) {
       document.title = `Address ${addressOrName} | Otterscan`;
@@ -81,29 +75,11 @@ const AddressMainPage: React.FC<AddressMainPageProps> = ({}) => {
       ) : (
         checksummedAddress && (
           <>
-            <StandardSubtitle>
-              <div className="flex space-x-2 items-baseline">
-                <Blockies
-                  className="self-center rounded"
-                  seed={checksummedAddress.toLowerCase()}
-                  scale={3}
-                />
-                <span>Address</span>
-                <span className="font-address text-base text-gray-500">
-                  {checksummedAddress}
-                </span>
-                <Copy value={checksummedAddress} rounded />
-                {/* Only display faucets for testnets who actually have any */}
-                {network === "testnet" && faucets && faucets.length > 0 && (
-                  <Faucet address={checksummedAddress} rounded />
-                )}
-                {isENS && (
-                  <span className="rounded-lg px-2 py-1 bg-gray-200 text-gray-500 text-xs">
-                    ENS: {addressOrName}
-                  </span>
-                )}
-              </div>
-            </StandardSubtitle>
+            <AddressSubtitle
+              addressOrName={addressOrName}
+              checksummedAddress={checksummedAddress}
+              isENS={isENS}
+            />
             <Tab.Group>
               <Tab.List className="flex space-x-2 border-l border-r border-t rounded-t-lg bg-white">
                 <NavTab href={`/address/${addressOrName}`}>Overview</NavTab>
---
+++ src/address/AddressSubtitle.tsx
@@ -0,0 +1,49 @@
+import { FC } from "react";
+import Blockies from "react-blockies";
+import StandardSubtitle from "../StandardSubtitle";
+import Copy from "../components/Copy";
+import Faucet from "../components/Faucet";
+import { useChainInfo } from "../useChainInfo";
+import { ChecksummedAddress } from "../types";
+
+type AddressSubtitleProps = {
+  checksummedAddress: ChecksummedAddress;
+  isENS: boolean | undefined;
+  addressOrName: string;
+};
+
+const AddressSubtitle: FC<AddressSubtitleProps> = ({
+  checksummedAddress,
+  isENS,
+  addressOrName,
+}) => {
+  const { network, faucets } = useChainInfo();
+
+  return (
+    <StandardSubtitle>
+      <div className="flex space-x-2 items-baseline">
+        <Blockies
+          className="self-center rounded"
+          seed={checksummedAddress.toLowerCase()}
+          scale={3}
+        />
+        <span>Address</span>
+        <span className="font-address text-base text-gray-500">
+          {checksummedAddress}
+        </span>
+        <Copy value={checksummedAddress} rounded />
+        {/* Only display faucets for testnets who actually have any */}
+        {network === "testnet" && faucets && faucets.length > 0 && (
+          <Faucet address={checksummedAddress} rounded />
+        )}
+        {isENS && (
+          <span className="rounded-lg px-2 py-1 bg-gray-200 text-gray-500 text-xs">
+            ENS: {addressOrName}
+          </span>
+        )}
+      </div>
+    </StandardSubtitle>
+  );
+};
+
+export default AddressSubtitle;
