c043c61fab41114f8d342917d19a684b223b74ae
Willian Mitsuda <wmitsuda@gmail.com>
2023-02-11 17:28:51 -0300

    Add stories

---
+++ .storybook/preview.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "@fontsource/fira-code/index.css";
+import "@fontsource/space-grotesk/index.css";
+import "@fontsource/roboto/index.css";
+import "@fontsource/roboto-mono/index.css";
+import "@fontsource/space-grotesk/files/space-grotesk-latin-400-normal.woff2";
+import "../src/index.css";
+
+export const parameters = {
+  backgrounds: {
+    default: "light",
+  },
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <Router>
+      <Story />
+    </Router>
+  ),
+];
