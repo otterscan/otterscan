72888aff1a4372976e45f671c883126d8b042df2
Willian Mitsuda <wmitsuda@gmail.com>
2023-02-11 18:33:48 -0300

    Add link-component stories

---
+++ src/consensus/components/ValidatorLink.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ValidatorLink from "./ValidatorLink";
+
+const meta = {
+  component: ValidatorLink,
+} satisfies Meta<typeof ValidatorLink>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const GenesisValidator: Story = {
+  args: {
+    validatorIndex: 0,
+  },
+};
+
+export const RecentMainnetValidatorIndex: Story = {
+  args: {
+    validatorIndex: 560_000,
+  },
+};
+
+export const SlashedValidator: Story = {
+  args: {
+    ...RecentMainnetValidatorIndex.args,
+    slashed: true,
+  },
+};
