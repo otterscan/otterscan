6ce3ec367dab114543aee5cdd14972cf43e3e92d
Willian Mitsuda <wmitsuda@gmail.com>
2023-02-13 16:34:07 -0300

    Add symbol support to FormattedBalance component

--- src/components/FormattedBalance.tsx
+++ src/components/FormattedBalance.tsx
@@ -1,23 +1,30 @@
-import React from "react";
+import { FC, memo } from "react";
 import { BigNumber } from "@ethersproject/bignumber";
 import { commify, formatUnits } from "@ethersproject/units";
 
 type FormatterBalanceProps = {
   value: BigNumber;
   decimals?: number;
+  symbol?: string | undefined;
 };
 
 // TODO: remove duplication with TransactionValue component
-const FormattedBalance: React.FC<FormatterBalanceProps> = ({
+const FormattedBalance: FC<FormatterBalanceProps> = ({
   value,
   decimals = 18,
+  symbol,
 }) => {
   const formatted = commify(formatUnits(value, decimals));
   const stripZeroDec = formatted.endsWith(".0")
     ? formatted.slice(0, formatted.length - 2)
     : formatted;
 
-  return <>{stripZeroDec}</>;
+  return (
+    <>
+      {stripZeroDec}
+      {symbol != undefined ? " " + symbol : ""}
+    </>
+  );
 };
 
-export default React.memo(FormattedBalance);
+export default memo(FormattedBalance);
