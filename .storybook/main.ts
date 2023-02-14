1c07c315f5f1ff4deabb868cd5ace487bf153add
Willian Mitsuda <wmitsuda@gmail.com>
2023-02-14 18:49:52 -0300

    Apply code formatter

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,19 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
-  "docs": {
-    "autodocs": "tag"
-  }
+  docs: {
+    autodocs: "tag",
+  },
 };
+
 export default config;
