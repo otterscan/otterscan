ce0f90aa6752fc72f7be3dd31db1baaf752f2fe1
Willian Mitsuda <wmitsuda@gmail.com>
2023-02-14 17:28:37 -0300

    Add stories for DecodedLogSignatures

--- src/transaction/decoder/DecodedLogSignature.tsx
+++ src/transaction/decoder/DecodedLogSignature.tsx
@@ -1,23 +1,23 @@
-import React from "react";
+import { FC, memo } from "react";
 import { EventFragment } from "@ethersproject/abi";
 
 type DecodedLogSignatureProps = {
   event: EventFragment;
 };
 
-const DecodedLogSignature: React.FC<DecodedLogSignatureProps> = ({ event }) => {
-  return (
-    <span>
-      <span className="font-bold text-blue-900">{event.name}</span>(
-      {event.inputs.map((input, i) => (
-        <span key={i}>
-          {i > 0 ? ", " : ""}
-          <span>{input.format ? input.format("full") : `${input.type} ${input.name}`}</span>
+const DecodedLogSignature: FC<DecodedLogSignatureProps> = ({ event }) => (
+  <span>
+    <span className="font-bold text-blue-900">{event.name}</span>(
+    {event.inputs.map((input, i) => (
+      <span key={i}>
+        {i > 0 ? ", " : ""}
+        <span>
+          {input.format ? input.format("full") : `${input.type} ${input.name}`}
         </span>
-      ))}
-      ){event.anonymous ? " anonymous" : ""}
-    </span>
-  );
-};
+      </span>
+    ))}
+    ){event.anonymous ? " anonymous" : ""}
+  </span>
+);
 
-export default React.memo(DecodedLogSignature);
+export default memo(DecodedLogSignature);
