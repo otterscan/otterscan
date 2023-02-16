dfbbbfa0ca4bb1b2e0ea7e401d69c475ec168e1a
Willian Mitsuda <wmitsuda@gmail.com>
2023-02-16 01:45:17 -0300

    Replace vite-react -> vite-react-swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
