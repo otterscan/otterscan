38ec6665038ec0e1d2bf715399bdd9808acd218b
Willian Mitsuda <wmitsuda@gmail.com>
2023-03-18 21:57:10 +0400

    Fix URL with ENS name in vite; fix #557

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteCompression from "vite-plugin-compression";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,6 @@ export default defineConfig({
     react(),
     viteCompression(),
     viteCompression({ algorithm: "brotliCompress" }),
+    pluginRewriteAll(),
   ],
 });
