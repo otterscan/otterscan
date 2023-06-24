3cf0d37b68ff3c4a173107bddeba72889f7a09a1
Willian Mitsuda <wmitsuda@gmail.com>
2023-06-24 18:24:23 -0300

    Add docs

--- src/useTitle.ts
+++ src/useTitle.ts
@@ -1,12 +1,18 @@
 import { useEffect } from "react";
 import { commify } from "@ethersproject/units";
 
-export const useBlockPageTitle = (blockNumberOrHash: number) => {
+/**
+ * Title for main block page.
+ */
+export const useBlockPageTitle = (blockNumber: number) => {
   useEffect(() => {
-    document.title = `Block #${commify(blockNumberOrHash)} | Otterscan`;
-  }, [blockNumberOrHash]);
+    document.title = `Block #${commify(blockNumber)} | Otterscan`;
+  }, [blockNumber]);
 };
 
+/**
+ * Page title for 1 page of transactions results for a block.
+ */
 export const useBlockTransactionsPageTitle = (blockNumber: number) => {
   useEffect(() => {
     if (blockNumber !== undefined) {
