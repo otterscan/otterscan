d206b30584e0216a0a5db38a801577972a51cc5b
sealer3 <125761775+sealer3@users.noreply.github.com>
2023-10-31 17:48:43 +0000

    Reduce margin starting at lg width (#1439)

--- src/components/StandardFrame.tsx
+++ src/components/StandardFrame.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from "react";
 
 const StandardFrame: React.FC<PropsWithChildren> = ({ children }) => (
-  <div className="grow bg-gray-100 px-3 md:px-9 pb-12 pt-3">{children}</div>
+  <div className="grow bg-gray-100 px-3 lg:px-9 pb-12 pt-3">{children}</div>
 );
 
 export default StandardFrame;
