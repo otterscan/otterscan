97f825bdc76cd7e35afbdb97b58b5b78099154c9
sealer3 <125761775+sealer3@users.noreply.github.com>
2023-11-18 20:23:46 +0000

    Add Cypress project ID and enable test recording

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    projectId: "rypdvn",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
