c29dff06cb66566c44c5f38c7a330ee6092e32b6
Willian Mitsuda <wmitsuda@gmail.com>
2023-11-25 19:13:48 -0300

    Beautify script

--- scripts/run-erigon-devnet-e2e-tests.sh
+++ scripts/run-erigon-devnet-e2e-tests.sh
@@ -1,2 +1,9 @@
-#!/usr/bin/env bash
-./erigon/build/bin/erigon --chain=dev --datadir=dev --http.api eth,erigon,trace,ots,ots2 --http.corsdomain "*" --http.vhosts "*" --mine --fakepow
+#!/usr/bin/env sh
+./erigon/build/bin/erigon \
+  --chain=dev \
+  --datadir=dev \
+  --http.api eth,erigon,trace,ots,ots2 \
+  --http.corsdomain "*" \
+  --http.vhosts "*" \
+  --mine \
+  --fakepow
