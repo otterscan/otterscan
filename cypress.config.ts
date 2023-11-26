bdf23a83a32440bd5e39f7f337c035d637bcd79c
Willian Mitsuda <wmitsuda@gmail.com>
2023-11-26 19:17:05 -0300

    Delete video for successful attempts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from "cypress";
+import fs from "fs";
 
 export default defineConfig({
   e2e: {
     projectId: "rypdvn",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on(
+        "after:spec",
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === "failed"),
+            );
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video);
+            }
+          }
+        },
+      );
     },
     baseUrl: "http://localhost:5173",
     video: true,
