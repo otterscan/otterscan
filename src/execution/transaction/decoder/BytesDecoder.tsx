06ecabfeb1a34728bc8f26db522f6113ffd6bc9e
sealer3 <125761775+sealer3@users.noreply.github.com>
2023-11-27 01:49:56 +0000

    Prevent bytes length label from being selected

--- src/execution/transaction/decoder/BytesDecoder.tsx
+++ src/execution/transaction/decoder/BytesDecoder.tsx
@@ -6,8 +6,9 @@ type BytesDecoderProps = {
 
 const BytesDecoder: FC<BytesDecoderProps> = ({ r }) => (
   <span className="font-code">
-    {r.toString()}{" "}
-    <span className="font-sans text-xs text-gray-400">
+    {r.toString()}
+    <span className="select-none"> </span>
+    <span className="font-sans text-xs text-gray-400 select-none">
       {r.toString().length / 2 - 1}{" "}
       {r.toString().length / 2 - 1 === 1 ? "byte" : "bytes"}
     </span>
