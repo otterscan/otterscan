3110066cb2ef705d0e909c7f1c392af13a62d967
sealer3 <125761775+sealer3@users.noreply.github.com>
2023-12-09 04:04:24 +0000

    Add DisplayInteger story

---
+++ src/components/DisplayInteger.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from "@storybook/react";
+import DisplayInteger from "./DisplayInteger";
+
+const meta = {
+  component: DisplayInteger,
+} satisfies Meta<typeof DisplayInteger>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const One: Story = {
+  args: {
+    numberStr: "1",
+  },
+};
+
+export const MultipleCommas: Story = {
+  args: {
+    numberStr: "1234567",
+  },
+};
+
+export const NegativeNumber: Story = {
+  args: {
+    numberStr: "-123456789",
+  },
+};
+
+export const EighteenDecimalsWithShadow: Story = {
+  args: {
+    numberStr: "11000000000000000000",
+    decimalHint: true,
+  },
+};
