34fecf63e2a8b7a3dc2763ee0cf50f2a6fcba109
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-02-14 05:05:16 +0000

    Reduce horizontal padding in standard table

--- src/components/StandardTBody.tsx
+++ src/components/StandardTBody.tsx
@@ -1,7 +1,7 @@
 import { FC, PropsWithChildren } from "react";
 
 const StandardTBody: FC<PropsWithChildren> = ({ children }) => (
-  <tbody className="[&>tr>td]:truncate [&>tr>td]:px-2 [&>tr>td]:py-3 [&>tr]:border-t [&>tr]:border-gray-200 hover:[&>tr]:bg-skin-table-hover">
+  <tbody className="[&>tr>td]:truncate [&>tr>td]:px-1 [&>tr>td]:py-3 [&>tr]:border-t [&>tr]:border-gray-200 hover:[&>tr]:bg-skin-table-hover">
     {children}
   </tbody>
 );
