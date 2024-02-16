470870db8195165e02f5c875e1d4db372350b667
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-02-16 02:08:06 +0000

    Add padding to table rows

--- src/components/StandardTHead.tsx
+++ src/components/StandardTHead.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from "react";
 
 const StandardTHead: FC<PropsWithChildren> = ({ children }) => (
   <thead>
-    <tr className="bg-gray-100 text-gray-500 [&>th]:truncate [&>th]:px-1 [&>th]:py-2">
+    <tr className="bg-gray-100 text-gray-500 [&>th]:truncate [&>th:first-child]:pl-2 [&>th:last-child]:pr-2 [&>th]:px-1 [&>th]:py-2">
       {children}
     </tr>
   </thead>
