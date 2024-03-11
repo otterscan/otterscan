8ec9e84fe1410653aa9017b45accf8503e954c64
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-03-11 05:11:38 +0000

    Add initial inline address labeling tool
    This tool uses localStorage to store address label changes and has support
    for loading labels from external lists once a UI is created for it.

--- src/execution/address/AddressSubtitle.tsx
+++ src/execution/address/AddressSubtitle.tsx
@@ -1,6 +1,6 @@
-import { faTag } from "@fortawesome/free-solid-svg-icons";
+import { faPencil, faTag, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FC, useContext } from "react";
+import { FC, useContext, useState } from "react";
 import Blockies from "react-blockies";
 import Copy from "../../components/Copy";
 import Faucet from "../../components/Faucet";
@@ -10,6 +10,7 @@ import { useResolvedAddress } from "../../useResolvedAddresses";
 import { RuntimeContext } from "../../useRuntime";
 import { AddressAwareComponentProps } from "../types";
 import AddressAttributes from "./AddressAttributes";
+import EditableAddressTag from "./EditableAddressTag";
 
 type AddressSubtitleProps = AddressAwareComponentProps & {
   isENS: boolean | undefined;
@@ -36,6 +37,8 @@ const AddressSubtitle: FC<AddressSubtitleProps> = ({
     resolvedNameTrusted = true;
   }
 
+  const [editingAddressTag, setEditingAddressTag] = useState<boolean>(false);
+
   return (
     <StandardSubtitle>
       <div className="flex items-baseline space-x-2">
@@ -55,11 +58,31 @@ const AddressSubtitle: FC<AddressSubtitleProps> = ({
         {/* Only display faucets for testnets who actually have any */}
         {faucets && faucets.length > 0 && <Faucet address={address} rounded />}
         {config?.experimental && <AddressAttributes address={address} full />}
-        {resolvedName && resolvedNameTrusted && (
-          <div className="rounded-lg bg-gray-200 px-2 py-1 text-sm text-gray-500">
-            <FontAwesomeIcon icon={faTag} size="1x" /> {resolvedName}
+        {resolvedName && resolvedNameTrusted && !editingAddressTag && (
+          <div className="rounded-lg bg-gray-200 px-2 py-1 text-sm text-gray-500 text-nowrap">
+            <FontAwesomeIcon icon={faTag} size="1x" />
+            <span className="pl-1 text-nowrap">{resolvedName}</span>
           </div>
         )}
+        <div className="flex flex-no-wrap space-x-1">
+          {editingAddressTag && (
+            <EditableAddressTag
+              address={address}
+              defaultTag={resolvedName}
+              editedCallback={(address: string) => setEditingAddressTag(false)}
+            />
+          )}
+          <button
+            className={`flex-no-wrap flex items-center justify-center space-x-1 self-center text-gray-500 focus:outline-none transition-shadows h-7 w-7 rounded-full bg-gray-200 text-xs transition-colors hover:bg-gray-500 hover:text-gray-200 hover:shadow`}
+            title={editingAddressTag ? "Cancel changes" : "Edit address label"}
+            onClick={() => setEditingAddressTag(!editingAddressTag)}
+          >
+            <FontAwesomeIcon
+              icon={editingAddressTag ? faTimes : faPencil}
+              size="1x"
+            />
+          </button>
+        </div>
       </div>
     </StandardSubtitle>
   );
