6e8bb0333ae759ad0364b958226b3a638fe4fa91
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-03-17 23:10:32 +0000

    Add turn down symbol to Optimism L1 deposit transactions in list (#1883)
    * Add turn down symbol to Optimism L1 deposit transactions in list
    
    * Remove deposit default as false
    
    * Change deposit icon color to emerald-600
    
    * Add deposit transaction Story for TransactionLink

--- src/search/TransactionItem.tsx
+++ src/search/TransactionItem.tsx
@@ -57,6 +57,7 @@ const TransactionItem: React.FC<TransactionItemProps> = ({
             txHash={tx.hash}
             fail={tx.status === 0}
             blob={tx.type === 3}
+            deposit={tx.type === 126}
           />
         </td>
         {/* Set both min and max widths to reduce column width changes when items of different lengths appear */}
