63dabd4324b178cc3aea61320a76fc9be9a9ae5f
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-03-20 04:37:58 +0000

    Add Wrapped ETH address to mainnet defaults

--- src/usePriceOracle.ts
+++ src/usePriceOracle.ts
@@ -41,6 +41,7 @@ const tokenEquivMap = new Map<
 ]);
 
 const mainnetPriceOracleInfo: PriceOracleInfo = {
+  wrappedEthAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
   uniswapV2: {
     factoryAddress: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
   },
