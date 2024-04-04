b5065c28384f156f10a6a6093e91cb4293327a5c
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-04-05 00:38:34 +0100

    Remove unused props

--- src/execution/address/contract/RawABI.tsx
+++ src/execution/address/contract/RawABI.tsx
@@ -2,10 +2,6 @@ import { FC, memo } from "react";
 import { ABIAwareComponentProps } from "../../types";
 import HighlightedSource from "./HighlightedSource";
 
-type HighlightedSolidityProps = {
-  source?: string | null;
-};
-
 const RawABI: FC<ABIAwareComponentProps> = ({ abi }) => {
   return (
     <div className="h-60 w-full border font-code text-base overflow-auto">
