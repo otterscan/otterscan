cd81017a7553e48a5813bbd788d6aa4a6afd00c5
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-04-25 01:47:43 +0100

    Reduce vertical padding

--- src/components/ElementDiff.tsx
+++ src/components/ElementDiff.tsx
@@ -9,7 +9,7 @@ const ElementDiff: React.FC<ElementDiffProps> = ({ oldElem, newElem }) => {
   return (
     <div className="flex flex-col rounded overflow-hidden">
       {oldElem !== null && (
-        <div className="bg-opacity-10 bg-red-500 p-2">
+        <div className="bg-opacity-10 bg-red-500 px-2 py-1">
           <div className="flex items-start">
             <span className="text-gray-500 mr-4 font-data select-none">-</span>
             {oldElem}
@@ -17,7 +17,7 @@ const ElementDiff: React.FC<ElementDiffProps> = ({ oldElem, newElem }) => {
         </div>
       )}
       {newElem !== null && (
-        <div className="bg-opacity-20 bg-green-300 p-2">
+        <div className="bg-opacity-20 bg-green-300 px-2 py-1">
           <div className="flex items-start">
             <span className="text-gray-500 mr-4 font-data select-none">+</span>
             {newElem}
