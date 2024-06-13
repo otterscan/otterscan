dd412c0ccc681360c93c8f1eddb0d09e02580860
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-06-13 22:44:49 +0000

    Add Docker env var for entire Otterscan config (#2104)
    * Add OTTERSCAN_CONFIG Docker env var
    
    * Add DISABLE_CONFIG_OVERWRITE per feature request

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-# Build a json from container init params
-PARAMS=$(jq -n \
-  --arg erigonURL "$ERIGON_URL" \
-  --arg beaconAPI "$BEACON_API_URL" \
-  --arg assetsURLPrefix "" \
-  --arg experimental "$OTS2" \
-  '{
-    erigonURL: $erigonURL,
-    beaconAPI: $beaconAPI,
-    assetsURLPrefix: $assetsURLPrefix,
-    experimental: $experimental,
-  }')
+# If complete config is provided, use it
+if [ "$OTTERSCAN_CONFIG" ]; then
+  PARAMS="$OTTERSCAN_CONFIG"
+else
+  # Build config JSON from container init params
+  PARAMS=$(jq -n \
+    --arg erigonURL "$ERIGON_URL" \
+    --arg beaconAPI "$BEACON_API_URL" \
+    --arg assetsURLPrefix "$ASSETS_URL_PREFIX" \
+    --arg experimental "$OTS2" \
+    '{
+      erigonURL: $erigonURL,
+      beaconAPI: $beaconAPI,
+      assetsURLPrefix: $assetsURLPrefix,
+      experimental: $experimental,
+    }')
+fi
 
 # Overwrite base image config.json with our own and let nginx do the rest
-echo $PARAMS > /usr/share/nginx/html/config.json
+if [ ! "$DISABLE_CONFIG_OVERWRITE" ]; then
+  echo $PARAMS > /usr/share/nginx/html/config.json
+fi
 exec nginx -g "daemon off;"
