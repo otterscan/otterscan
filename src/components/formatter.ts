53d3971144a6666822e07843a1fb85305eb05949
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-11-02 15:28:38 +0000

    Limit gas price decimal places

--- src/components/formatter.ts
+++ src/components/formatter.ts
@@ -7,3 +7,28 @@ export const formatValue = (value: BigNumberish, decimals: number): string => {
     ? formatted.slice(0, formatted.length - 2)
     : formatted;
 };
+
+/**
+ * Trims a BigInt to either a minimum number of significant figures or decimal
+ * places.
+ *
+ * @param {BigInt} num - The number to trim
+ * @param {number} decimals - Decimal places num has
+ * @param {number} minSigFigs - Minimum significant figures
+ * @param {number} minDecimals - Minimum decimal places
+ * @returns {BigInt} The trimmed number
+ */
+export function trimToPrecision(
+  num: bigint,
+  decimals: number,
+  minSigFigs: number,
+  minDecimals: number,
+): bigint {
+  const totalFigures = num.toString().length;
+  const figuresToCut = Math.max(
+    Math.min(decimals - minDecimals, totalFigures - minSigFigs),
+    0,
+  );
+  const roundingValue = 10n ** BigInt(figuresToCut);
+  return (num / roundingValue) * roundingValue;
+}
