f049c42663c594514d0b08cdad586a823721acb2
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-12-17 23:16:35 +0000

    Show whatsabi selectors when assetsURLPrefix is not configured (#2557)
    * Show whatsabi selectors when assetsURLPrefix is not configured
    
    * Remove dead code
    
    * Update Cypress dependencies install for Ubuntu 24.04

--- src/sourcify/useWhatsabi.ts
+++ src/sourcify/useWhatsabi.ts
@@ -28,60 +28,57 @@ export const useWhatsabiMetadata = (
 
 function whatsabiFetcher(
   provider: JsonRpcApiProvider,
-  assetsURLPrefix: string | undefined,
+  assetsURLPrefix: string | null | undefined,
 ): Fetcher<Match | null | undefined, ["whatsabi", ChecksummedAddress, bigint]> {
   return async ([_, address, chainId]) => {
-    if (assetsURLPrefix !== undefined) {
-      const code = await queryClient.fetchQuery(
-        getCodeQuery(provider, address, "latest"),
-      );
-      const selectors = whatsabi.selectorsFromBytecode(code);
-      const decodedFunctions: (string | null)[] = await Promise.all(
-        selectors.map(async (selector) => {
-          try {
-            const result = await fetch(
-              fourBytesURL(assetsURLPrefix, selector.slice(2)),
-            );
-            if (!result.ok) {
-              throw new Error(
-                `4bytes fetch returned ${result.status} response`,
-              );
-            }
-            const text = await result.text();
-            const sig = text.split(";")[0];
-            if (sig.length > 0) {
-              return `function ${sig} external view`;
-            }
-          } catch (e) {}
+    const code = await queryClient.fetchQuery(
+      getCodeQuery(provider, address, "latest"),
+    );
+    const selectors = whatsabi.selectorsFromBytecode(code);
+    const decodedFunctions: (string | null)[] = await Promise.all(
+      selectors.map(async (selector) => {
+        if (!assetsURLPrefix) {
           return null;
-        }),
-      );
-      const unknownSelectors = selectors.filter(
-        (selector, i) => decodedFunctions[i] === null,
-      );
-      const functions: string[] = decodedFunctions
-        .filter((sig) => sig !== null)
-        .sort() as string[];
-      const int = Interface.from([...functions]);
-      const abi = JSON.parse(int.formatJson());
-      const metadata = {
-        version: "Unknown",
-        language: "Unknown",
-        compiler: { version: "Unknown" },
-        sources: {},
-        // settings: {remappings: [], compilationTarget: {}, libraries: {}},
-        output: {
-          abi,
-        },
-      };
+        }
+        try {
+          const result = await fetch(
+            fourBytesURL(assetsURLPrefix, selector.slice(2)),
+          );
+          if (!result.ok) {
+            throw new Error(`4bytes fetch returned ${result.status} response`);
+          }
+          const text = await result.text();
+          const sig = text.split(";")[0];
+          if (sig.length > 0) {
+            return `function ${sig} external view`;
+          }
+        } catch (e) {}
+        return null;
+      }),
+    );
+    const unknownSelectors = selectors.filter(
+      (selector, i) => decodedFunctions[i] === null,
+    );
+    const functions: string[] = decodedFunctions
+      .filter((sig) => sig !== null)
+      .sort() as string[];
+    const int = Interface.from([...functions]);
+    const abi = JSON.parse(int.formatJson());
+    const metadata = {
+      version: "Unknown",
+      language: "Unknown",
+      compiler: { version: "Unknown" },
+      sources: {},
+      // settings: {remappings: [], compilationTarget: {}, libraries: {}},
+      output: {
+        abi,
+      },
+    };
 
-      return {
-        type: MatchType.WHATSABI_GUESS,
-        metadata: metadata,
-        unknownSelectors,
-      };
-    }
-
-    return null;
+    return {
+      type: MatchType.WHATSABI_GUESS,
+      metadata: metadata,
+      unknownSelectors,
+    };
   };
 }
