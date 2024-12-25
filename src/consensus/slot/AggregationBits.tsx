c73b2c8b87ef6224c9cb30241fcd2bb7599eda8c
sealer3 <125761775+sealer3@users.noreply.github.com>
2024-12-25 15:52:26 +0000

    Make aggregation bits grid columns responsive

--- src/consensus/slot/AggregationBits.tsx
+++ src/consensus/slot/AggregationBits.tsx
@@ -8,7 +8,7 @@ type AggregationBitsProps = {
 const AggregationBits: FC<AggregationBitsProps> = ({ hex }) => {
   const bm = Array.from(getBytes(hex));
   return (
-    <div className="grid w-fit grid-cols-8 gap-2 font-hash">
+    <div className="grid w-fit grid-cols-4 sm:grid-cols-6 md:grid-cols-8 gap-2 font-hash">
       {bm.map((b, i) => (
         <Bitmap key={i} b={b} />
       ))}
