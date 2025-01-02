60450f04e140487b4d1a0fbc90f31b861bf8c381
sealer3 <125761775+sealer3@users.noreply.github.com>
2025-01-02 02:47:34 +0000

    Add additional vertical space between elements in vertical stack mode

--- src/components/InfoRow.tsx
+++ src/components/InfoRow.tsx
@@ -6,7 +6,7 @@ type InfoRowProps = React.PropsWithChildren<{
 
 const InfoRow: React.FC<InfoRowProps> = ({ title, children }) => (
   <div className="sm:grid sm:grid-cols-4 py-3 sm:py-4 text-sm">
-    <div className="mb-1 sm:mb-auto">{title}:</div>
+    <div className="mb-2 sm:mb-auto">{title}:</div>
     <div className="sm:col-span-3">{children}</div>
   </div>
 );
