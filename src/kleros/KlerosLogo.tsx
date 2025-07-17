9908315b5945b87c6f36d0e628ee9ad43379a2ef
gmkung <guangmian@gmail.com>
2025-07-17 22:59:22 +0800

    Fixed Kleros logo for dark/light mode

--- src/kleros/KlerosLogo.tsx
+++ src/kleros/KlerosLogo.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import klerosSymbol from "./kleros-symbol-white-flat.svg";
+import klerosSymbol from "./kleros-symbol.svg";
+import klerosSymbolWhite from "./kleros-symbol-white-flat.svg";
 
 const KlerosLogo: React.FC = () => (
-  <img
-    className="h-4 w-4"
-    src={klerosSymbol}
-    alt="Kleros Scout"
-    title="Verified by Kleros Scout"
-  />
+  <>
+    <img
+      className="h-4 w-4 dark:hidden"
+      src={klerosSymbol}
+      alt="Kleros Scout"
+      title="Verified by Kleros Scout"
+    />
+    <img
+      className="h-4 w-4 hidden dark:block"
+      src={klerosSymbolWhite}
+      alt="Kleros Scout"
+      title="Verified by Kleros Scout"
+    />
+  </>
 );
 
 export default KlerosLogo;
