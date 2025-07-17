25419190488067916df96c307334b406ab650dd0
gmkung <guangmian@gmail.com>
2025-07-17 16:12:19 +0800

    Updated styling and logo

--- src/kleros/KlerosLogo.tsx
+++ src/kleros/KlerosLogo.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import klerosSymbol from "./kleros-symbol-white-flat.svg";
 
 const KlerosLogo: React.FC = () => (
   <img
-    className="h-4 w-4 rounded-full"
-    src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPGNpcmNsZSBjeD0iMzIiIGN5PSIzMiIgcj0iMzIiIGZpbGw9IiM1QzVGRkYiLz4KPHBhdGggZD0iTTIwIDI0TDMyIDEyTDQ0IDI0VjQwTDMyIDUyTDIwIDQwVjI0WiIgZmlsbD0id2hpdGUiLz4KPHBhdGggZD0iTTI4IDI4TDMyIDI0TDM2IDI4VjM2TDMyIDQwTDI4IDM2VjI4WiIgZmlsbD0iIzVDNUZGRiIvPgo8L3N2Zz4="
+    className="h-4 w-4"
+    src={klerosSymbol}
     alt="Kleros Scout"
     title="Verified by Kleros Scout"
   />
