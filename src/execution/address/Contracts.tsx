5896b7c1fd6a4f5b6a5941cdf5933075b857aa4e
sealer3 <125761775+sealer3@users.noreply.github.com>
2025-08-14 08:48:39 +0100

    Prevent x-overflow

--- src/execution/address/Contracts.tsx
+++ src/execution/address/Contracts.tsx
@@ -160,7 +160,7 @@ const Contracts: React.FC<ContractsProps> = ({ checksummedAddress, match }) => {
                       initial={{ height: 0 }}
                       animate={{ height: "auto" }}
                       transition={{ duration: 0.5 }}
-                      className="overflow-y-hidden"
+                      className="overflow-y-clip"
                     >
                       <ContractVerificationSteps address={checksummedAddress} />
                     </motion.div>
