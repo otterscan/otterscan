b3dd1d1709325f33374b7f051fcee1119b89db55
gmkung <guangmian@gmail.com>
2025-08-25 22:09:05 +0800

    Fix TypeScript error: remove title prop from ExternalLink, clean up SVG icon

--- src/kleros/KlerosTagBadge.tsx
+++ src/kleros/KlerosTagBadge.tsx
@@ -28,12 +28,9 @@ const KlerosTagBadge: React.FC<KlerosTagBadgeProps> = ({
         >
           {displayName}
         </span>
+
         {tag.website_link && !compact && (
-          <ExternalLink href={tag.website_link} title="Visit project website">
-            <svg className="h-3 w-3" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-              <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-            </svg>
+          <ExternalLink href={tag.website_link}>
           </ExternalLink>
         )}
       </div>
