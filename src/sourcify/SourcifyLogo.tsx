7e82ec3d2619c35568d2b872f31080404da668dc
sealer3 <125761775+sealer3@users.noreply.github.com>
2025-09-08 16:50:27 +0100

    Fix inner check white circle

--- src/sourcify/SourcifyLogo.tsx
+++ src/sourcify/SourcifyLogo.tsx
@@ -23,7 +23,7 @@ const SourcifyLogo: React.FC<SourcifyLogoProps> = ({ locallyVerified }) => {
         <div title={title}>
           {/* Draws a white circle behind to ensure the checkmark is white */}
           <FontAwesomeIcon
-            className="absolute bottom-[0.4px] right-[-1.2px] ml-1 text-white dark:text-black text-[4.6pt]"
+            className="absolute bottom-[0.2px] right-[-1.1px] ml-1 text-white dark:text-black text-[4pt]"
             icon={faCircle}
           />
           <FontAwesomeIcon
