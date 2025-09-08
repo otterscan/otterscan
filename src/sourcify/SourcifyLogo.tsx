6c58d4837cfae1586a622a2eb60bdfaa0ae228de
sealer3 <125761775+sealer3@users.noreply.github.com>
2025-09-08 16:08:14 +0100

    Move right and match Sourcify verified icon size ratio

--- src/sourcify/SourcifyLogo.tsx
+++ src/sourcify/SourcifyLogo.tsx
@@ -23,11 +23,11 @@ const SourcifyLogo: React.FC<SourcifyLogoProps> = ({ locallyVerified }) => {
         <div title={title}>
           {/* Draws a white circle behind to ensure the checkmark is white */}
           <FontAwesomeIcon
-            className="absolute bottom-[0.5px] right-[-1.5px] ml-1 text-white dark:text-black text-[5.6pt]"
+            className="absolute bottom-[0.5px] right-[-3.2px] ml-1 text-white dark:text-black text-[5.1pt]"
             icon={faCircle}
           />
           <FontAwesomeIcon
-            className="absolute bottom-0 right-[-2px] text-emerald-500 text-[6pt]"
+            className="absolute bottom-0 right-[-3.5px] text-emerald-500 text-[5.5pt]"
             icon={faCheckCircle}
           />
         </div>
